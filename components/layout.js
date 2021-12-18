a1191fcf0b75db996b19af76a4c9b24cbadc878f
bpozniak <benjaminpozniak@gmail.com>
2021-12-17 21:38:25 -0500

    navbar was still under layout but removed

--- components/layout.js
+++ components/layout.js
@@ -3,12 +3,10 @@ import React from 'react'
 export default function SimpleLayout(props) {
   return (
     <>
-      <Navbar />
       <main role="main">
         {props.preContainer && props.preContainer}
         <div className="album py-5 bg-light">
           <div className="container">
-            <Navbar />
             {props.children}
           </div>
         </div>
@@ -16,13 +14,3 @@ export default function SimpleLayout(props) {
     </>
   )
 }
-
-// export default function Layout({ children }) {
-//   return (
-//     <>
-//       <Navbar />
-//       <main>{children}</main>
-//       <Footer />
-//     </>
-//   )
-// }
